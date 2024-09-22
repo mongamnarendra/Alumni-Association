d35e679baa0dbc86e7d7100f7d75301d44412079
narendramongam <mongamnarendra@gmail.com>
2024-09-22 17:08:36 +0530

    css is added

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let currentSlide = 0;  
-    const slides = document.querySelectorAll('.slide');  
-    const totalSlides = slides.length;  
+function slideRight() {
+    
+    document.getElementById("imag").src="https://img.freepik.com/free-photo/idea-brainstorm-creative-planning-success-concept_53876-64983.jpg?w=826&t=st=1726942603~exp=1726943203~hmac=2ea09d00916b1063b2cfb531c35b7e8e61ea9bcf6f0c358d52a375c58e70503a";
+}
 
-    function updateCarousel() {
-        slides.forEach((slide, index) => {
-            slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;
-        });
-        updateIndicators(); // Update indicators on slide change
-    }
-
-    function updateIndicators() {
-        const indicators = document.querySelectorAll('.indicator');
-        indicators.forEach((indicator, index) => {
-            indicator.classList.toggle('active', index === currentSlide);
-        });
-    }
-
-    document.getElementById('next').addEventListener('click', () => {
-        currentSlide = (currentSlide + 1) % totalSlides;  
-        updateCarousel();
-    });
-
-    document.getElementById('prev').addEventListener('click', () => {
-        currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;  
-        updateCarousel();
-    });
-
-    // Start the carousel
-    updateCarousel();
-});
+function slideLeft() {
+    document.getElementById("imag").src="https://img.freepik.com/free-photo/happy-student-with-graduation-hat-diploma-grey_231208-12981.jpg?w=996&t=st=1726941928~exp=1726942528~hmac=bfcc61b00e05ea336ce79d87a08a783905d2bf9349d14a708cf89fd7b749f240"
+}
